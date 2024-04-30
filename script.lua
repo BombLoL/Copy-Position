6a2504d56471a70b0a3355db2a01b6dd2809aa3a
BombLoL <137166238+BombLoL@users.noreply.github.com>
2024-04-30 16:58:51 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -31,6 +31,7 @@ Frame.BorderSizePixel = 0
 Frame.Position = UDim2.new(0.5, 0, 0.5, 0)
 Frame.Size = UDim2.new(0, 257, 0, 306)
 Frame.ZIndex = 999999999
+Frame.Draggable = true
 
 UICorner.CornerRadius = UDim.new(0, 4)
 UICorner.Parent = Frame
@@ -173,59 +174,3 @@ local function ZBKRK_fake_script() -- reset.onClick
 	end)
 end
 coroutine.wrap(ZBKRK_fake_script)()
-local function NJQUP_fake_script() -- Frame.ajjajajajaj 
-	local UserInputService = game:GetService("UserInputService")
-	local runService = (game:GetService("RunService"));
-
-	local gui = script.Parent
-
-	local dragging
-	local dragInput
-	local dragStart
-	local startPos
-
-	function Lerp(a, b, m)
-		return a + (b - a) * m
-	end;
-
-	local lastMousePos
-	local lastGoalPos
-	local DRAG_SPEED = (8); -- // The speed of the UI darg.
-	function Update(dt)
-		if not (startPos) then return end;
-		if not (dragging) and (lastGoalPos) then
-			gui.Position = UDim2.new(startPos.X.Scale, Lerp(gui.Position.X.Offset, lastGoalPos.X.Offset, dt * DRAG_SPEED), startPos.Y.Scale, Lerp(gui.Position.Y.Offset, lastGoalPos.Y.Offset, dt * DRAG_SPEED))
-			return 
-		end;
-
-		local delta = (lastMousePos - UserInputService:GetMouseLocation())
-		local xGoal = (startPos.X.Offset - delta.X);
-		local yGoal = (startPos.Y.Offset - delta.Y);
-		lastGoalPos = UDim2.new(startPos.X.Scale, xGoal, startPos.Y.Scale, yGoal)
-		gui.Position = UDim2.new(startPos.X.Scale, Lerp(gui.Position.X.Offset, xGoal, dt * DRAG_SPEED), startPos.Y.Scale, Lerp(gui.Position.Y.Offset, yGoal, dt * DRAG_SPEED))
-	end;
-
-	gui.InputBegan:Connect(function(input)
-		if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
-			dragging = true
-			dragStart = input.Position
-			startPos = gui.Position
-			lastMousePos = UserInputService:GetMouseLocation()
-
-			input.Changed:Connect(function()
-				if input.UserInputState == Enum.UserInputState.End then
-					dragging = false
-				end
-			end)
-		end
-	end)
-
-	gui.InputChanged:Connect(function(input)
-		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
-			dragInput = input
-		end
-	end)
-
-	runService.Heartbeat:Connect(Update)
-end
-coroutine.wrap(NJQUP_fake_script)()
